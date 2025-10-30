63ade681633dc9ac88adb5cc71885b16006c787d
Abraham Mendez <jesusmenfig@gmail.com>
2025-10-30 13:16:50 -0700

    Se realizo el layout de lista

--- app/src/main/java/com/example/appunidad02/ListaFragment.kt
+++ app/src/main/java/com/example/appunidad02/ListaFragment.kt
@@ -5,55 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private lateinit var btnAgregarAlumno : FloatingActionButton
+private lateinit var bottomNavigationView: BottomNavigationView
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class ListaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista, container, false)
+        val view = inflater.inflate(R.layout.fragment_lista, container, false)
+
+        iniciarComponentes(view)
+        eventosClic()
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    fun iniciarComponentes(view : View){
+        btnAgregarAlumno = view.findViewById(R.id.btnAgregarAlumno)
+        bottomNavigationView = view.findViewById(R.id.btnNavegador)
+
+
     }
+
+    fun eventosClic(){
+        btnAgregarAlumno.setOnClickListener{
+            cambiarFragment(AlumnosFragment())
+        }
+    }
+
+    public fun cambiarFragment(fragment: Fragment){
+        parentFragmentManager.beginTransaction().replace(R.id.frmContenedor, fragment).addToBackStack(null).commit()
+        val bottomNav = requireActivity().findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(
+            R.id.btnNavegador
+        )
+        bottomNav.selectedItemId = R.id.btnAlumnos
+    }
+
 }
